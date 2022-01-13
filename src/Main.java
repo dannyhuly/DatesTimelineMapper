25cb4fb6dc4be7487548a6a02a12ed334d9e41fc
Daniel Huly <daniel.huly@broadcom.com>
2022-01-13 11:28:22 +0200

    Solution - DatesTimelineMapper

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 class Main {
 
@@ -11,7 +14,45 @@ class Main {
    * @param dates list of dates
    */
   private static Map<LocalDate, TimelineEvent> timelineEventsByDates(List<TimelineEvent> timeline, List<LocalDate> dates) {
-    return null;
+    List<LocalDate> sortedDates = dates
+        .stream()
+        .sorted((a, b) -> a.isAfter(b) ? 1 : -1)
+        .collect(Collectors.toList());
+
+    Collections.reverse(timeline);
+
+    Map<LocalDate, TimelineEvent> rolesByDatesRes = new HashMap<>();
+    int datesPointer = 0;
+    int timelinePointer = 0;
+
+    // before first event
+    while (datesPointer < sortedDates.size()) {
+      if (sortedDates.get(datesPointer).isBefore(timeline.get(timelinePointer).getEffectiveFrom()) ) {
+        rolesByDatesRes.put(sortedDates.get(datesPointer), new TimelineEvent("unknown", sortedDates.get(datesPointer)));
+        datesPointer++;
+      } else {
+        break;
+      }
+    }
+
+    // check dates between events
+    while (timelinePointer < timeline.size()-1) {
+      if (sortedDates.get(datesPointer).isAfter(timeline.get(timelinePointer).getEffectiveFrom()) &&
+          sortedDates.get(datesPointer).isBefore(timeline.get(timelinePointer+1).getEffectiveFrom())) {
+
+        rolesByDatesRes.put(sortedDates.get(datesPointer), timeline.get(timelinePointer));
+        datesPointer++;
+      }
+      timelinePointer++;
+    }
+
+    // after last events)
+    while (datesPointer < sortedDates.size()) {
+      rolesByDatesRes.put(sortedDates.get(datesPointer), timeline.get(timelinePointer));
+      datesPointer++;
+    }
+
+    return rolesByDatesRes;
   }
 
   public static void main(String[] args) {
